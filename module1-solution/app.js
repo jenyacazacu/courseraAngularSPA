11b10096adbb76c1585bd7561038c56800ce07d4
jenya cazacu <jenyacazacu@gmail.com>
2016-10-04 22:20:47 -0600

    added solution to module1

---
+++ module1-solution/app.js
@@ -0,0 +1,28 @@
+(function(){
+'use string';
+angular.module("WordCount", [])
+
+.controller("WordCountController",  WordCountController);
+WordCountController.$inject = ['$scope'];
+
+function WordCountController($scope) {
+  $scope.items = "";
+  $scope.message = "";
+  $scope.checkNumberOfItems = function(){
+      var items = $scope.items.split(',');
+      numberOfItems = 0;
+      items.forEach(function(word){
+        if(word.trim().length >= 2){
+          numberOfItems++;
+        }
+      });
+      if(numberOfItems === 0){
+        $scope.message = "Please provide some words";
+      } else if(numberOfItems >= 1 && numberOfItems <= 3) {
+        $scope.message = "Enjoy!";
+      } else {
+        $scope.message = "Too many";
+      }
+    }
+  }
+})();
